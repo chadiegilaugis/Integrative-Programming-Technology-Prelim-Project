39f6d48b995a8d3f32e42abf3568bb0e9b072791
Your Name <you@example.com>
2021-09-20 10:01:47 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" href="{{asset('mystyle/mystyle.css')}}">
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <script defer src="{{asset('js/bootstrap.min.js')}}"></script>
+
+    </head>
+    <body class="antialiased">
+        <div class="container mt-5">
+            <div class="d-flex justify-content-center align-items-center">
+                <h1 class="container d-flex justify-content-start align-items-center h3">Chadie Gil S. Augis</h1>
+                <nav class="container d-flex justify-content-end align-items-center">
+                    <h4>Integrative Programming & Technology 1</h4>
+                </nav>
+            </div>
+            <div class="d-flex justify-content-center m-5">
+                <h1>Prelim Project</h1>
+            </div>
+
+            <div class="d-flex justify-content-center p-3">
+                <h4 class="p-2">About |</h2>
+                <a class="p-2 h5" href="{{url('/')}}">Home</a>
+                <a class="p-2 h5"href="{{url('/contact')}}">Contact</a>
+            </div>
+            <div class="d-flex justify-content-center p-3 display-1 m-5">
+                Welcome to About page
+            </div>
+        </div>
+    </body>
+</html>
